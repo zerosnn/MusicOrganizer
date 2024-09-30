19f2d9af24ca3a21d03e2d799edbc290080c2401
zerosnn <163860841+zerosnn@users.noreply.github.com>
2024-10-01 05:38:20 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,24 +2,18 @@ public class Main {
     public static void main(String[] args) {
         MusicManager manager = new MusicManager();
 
-        // Menambahkan beberapa lagu dengan artisnya
         manager.addTrack("Song 1", "Artist A");
         manager.addTrack("Song 2", "Artist B");
         manager.addTrack("Song 3", "Artist C");
 
-        // Menampilkan semua lagu
         manager.listAllTracks();
 
-        // Memulai pemutaran lagu ke-2
         manager.playTrack(2);
 
-        // Menghentikan pemutaran lagu
         manager.stopTrack();
 
-        // Coba hentikan lagi (tidak ada lagu yang diputar)
         manager.stopTrack();
 
-        // Menampilkan jumlah lagu
         System.out.println("Total number of tracks: " + manager.getTrackCount());
     }
 }
